ac902d34cd65b760a7f4f582b24766a878e32fd8
Evie Marie Kolb <eviemariekolb@gmail.com>
2026-01-06 15:47:30 -0600

    add mui and loader to booking page

--- app/contact/booking/page.tsx
+++ app/contact/booking/page.tsx
@@ -1,9 +1,12 @@
+import { CircularProgress } from "@mui/material";
+import { useState } from "react"
+
 export default function BookingPage() {
+    const [loading, setLoading] = useState<boolean>(true);
 
-
-    return (
+    return loading ? <CircularProgress color="secondary" size="50%" /> : (
         <div>
-            <iframe src="https://app.usemotion.com/meet/evie-marie-kolb/booking" title="Evie Marie Kolb Booking Page" width="100%" height="840px" frameBorder="0"></iframe>
+            <iframe src="https://app.usemotion.com/meet/evie-marie-kolb/booking" title="Evie Marie Kolb Booking Page" width="100%" height="840px" frameBorder="0" onLoad={() => setLoading(false)}></iframe>
         </div>
     )
 }
