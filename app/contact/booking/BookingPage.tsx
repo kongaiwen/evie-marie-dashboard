b285939cb8642c6fdaf9d84b54e48841fcc79f7f
Evie Marie Kolb <eviemariekolb@gmail.com>
2026-01-06 15:19:36 -0600

    add booking page

---
+++ app/contact/booking/BookingPage.tsx
@@ -0,0 +1,9 @@
+export const BookingPage = () => {
+
+
+    return (
+        <div>
+            <iframe src="https://app.usemotion.com/meet/evie-marie-kolb/booking" title="Evie Marie Kolb Booking Page" width="100%" height="840px" frameBorder="0"></iframe>
+        </div>
+    )
+}
