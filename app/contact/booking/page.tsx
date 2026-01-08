4b1e994cf20e6a4305f56c3f58ff9d2237672535
Evie Marie Kolb <eviemariekolb@gmail.com>
2026-01-08 14:35:11 -0600

    style booking page and loader

--- app/contact/booking/page.tsx
+++ app/contact/booking/page.tsx
@@ -1,14 +1,27 @@
 'use client'
 
+import "./booking.css";
 import { CircularProgress } from "@mui/material";
-import { useState } from "react"
+import { useCallback, useState } from "react"
 
 export default function BookingPage() {
     const [loading, setLoading] = useState<boolean>(true);
 
-    return loading ? <CircularProgress color="secondary" size="50%" /> : (
-        <div>
-            <iframe src="https://app.usemotion.com/meet/evie-marie-kolb/booking" title="Evie Marie Kolb Booking Page" width="100%" height="840px" frameBorder="0" onLoad={() => setLoading(false)}></iframe>
+    const onLoad = useCallback(() => {
+        setTimeout(() => {
+            setLoading(false)
+        }, 500)
+    }, [])
+
+    return (
+        <div className="booking-container">
+            {!!loading && (
+                <div className="spinner-container">
+                    <h1>Loading Booking Page...</h1>
+                    <CircularProgress className="booking-spinner" color="inherit" size="8rem" />
+                </div>
+            )}
+            <iframe className="motion-iframe" src="https://app.usemotion.com/meet/evie-marie-kolb/booking" title="Evie Marie Kolb Booking Page" width="100%" height="100%" frameBorder="0" onLoad={onLoad}></iframe>
         </div>
     )
 }
